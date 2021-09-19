8cce237bd1eb6ca737093fd4b79b0d734b3bada9
maciek <maciek.pacut@gmail.com>
2021-09-19 10:59:14 +0200

    Header-only algorithm implementation

--- classify_reorder.hpp
+++ classify_reorder.hpp
@@ -1,76 +1,16 @@
-#include <iostream>
-#include <list>
-#include <vector>
-#include <algorithm>
-using namespace std;
+// fixme: ifdef
 
 
 // ----------------------------------------------------------------------------
 // ----------------------------------------------------------------------------
 // ----------------------------------------------------------------------------
-// Defining packets, rules, dependency relation and
-// packet-to-rule match predicate.
-struct MyPacket
-{
-    int ip_src;
-    int ip_dst;
-    int id;
-};
+// Implementation of classify-reorder algorithm
 
-struct MyRule
-{
-    int ip_src_min;
-    int ip_src_max;
-
-    int ip_dst_min;
-    int ip_dst_max;
-
-    int priority;
-    int action;
-};
-
-// Printing MyRule
-ostream& operator<<(ostream& os, const MyRule& r)
-{
-    os << r.priority;
-    return os;
-}
-
-
-struct IsDependencyPred
-{
-    bool operator()(const MyRule& r1, const MyRule& r2) const
-    {
-        // dependencies: (2,3), (3,4), (4,5)
-        // oversimplified
-        if (r1.priority == 5 && r2.priority == 4) return true;
-        if (r1.priority == 3 && r2.priority == 2) return true;
-        if (r1.priority == 4 && r2.priority == 3) return true;
-        return false;
-    }
-};
-
-struct MatchPacketPred
-{
-    MyPacket p;
-    MatchPacketPred(const MyPacket& _p) : p(_p) {}
-    bool operator()(const MyRule& r) const
-    {
-        // oversimplified
-        return p.id == r.priority;
-    }
-};
-
-// ----------------------------------------------------------------------------
-// ----------------------------------------------------------------------------
-// ----------------------------------------------------------------------------
-// Implementation of drlist
+// FIXME: concepts: Rule must be swappable
 
 // Abstracting from the Rule and Packet implementation and relying on
 // the user supplying the rule dependency check comparison.
 
-
-// FIXME: concepts: Rule must be swappable
 
 
 // Classify the given packet (return the matching rule) and then reorder the rules to speed up future classification
@@ -152,32 +92,3 @@ BidirectionalIterator classify_reorder(BidirectionalIterator first, Bidirectiona
     return found;
 }
 
-
-
-int main()
-{
-    vector<MyRule> original_list;
-
-    for(int i = 1; i < 6; ++i)
-    {
-        MyRule r;
-        r.priority = i;
-        original_list.push_back(r);
-    }
-
-
-    // Request the packet with id=5 for 5 times in the row.
-    for(int i = 0; i < 5; ++i)
-    {
-        // FIXME: print the list to see changes
-
-        MyPacket p;
-        p.id = 5;
-        
-        auto matching = classify_reorder<MyRule, MyPacket, MatchPacketPred, IsDependencyPred>(original_list.begin(), original_list.end(), p);
-
-        cout << "Found rule " << (*matching) << endl;
-    }
-
-    return 0;
-}
